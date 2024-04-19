b37b6a31ff10880e16a889d38986f829496f16db
bhlieberman <bhlieberman93@gmail.com>
2024-04-19 00:19:15 +0000

    remove commented out code and add more fields to out-data. the sequence data is currently serialized without any configuration (probably needs improvement)

--- stages/02_process_embl.py
+++ stages/02_process_embl.py
@@ -1,11 +1,12 @@
 from Bio import SeqIO
 import pandas as pd
+import pickle
 
 new_embl_records = SeqIO.parse("./download/miRNA.dat", "embl")
-# prev_embl_records = SeqIO.parse("./download/miRNA.dead", "embl")
-records = [{'name': record.name, 'description': record.description} for record in new_embl_records]
-# old_records = [{'name': record.name, 'description': record.description} for record in prev_embl_records]
+records = [{'name': record.name, 'description': record.description,
+            'AC': record.id, 'SQ': pickle.dumps(record.seq),
+            'molecule': record.annotations['molecule_type'],
+            'division': record.annotations['data_file_division']} 
+           for record in new_embl_records]
 df = pd.DataFrame.from_records(records)
-# dead = pd.DataFrame.from_records(old_records)
-df.to_parquet("./brick/miRNA.parquet")
-# df.to_parquet("./brick/miRNA_dead.parquet")
+df.to_parquet("./brick/miRNA.dat.parquet")
